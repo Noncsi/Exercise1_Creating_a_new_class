b865ad4a1c3ce47a6b85df8dc9721bb4beb994d7
Noncsi <lengyel.noemi96@gmail.com>
2017-06-10 23:29:39 +0200

    Create Person.cs

---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    class Person
+    {
+        public string name;
+        public DateTime birthDate;
+        public enum Genders { male, female };
+
+        public Person(string name, DateTime birthDate)
+        {
+            this.name = name;
+            this.birthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return "name: " + name + "\n" +
+                   "date of birth: " + birthDate;
+        }
+    }
+}
